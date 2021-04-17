d70bf435c867ec7b7016694fc6b8191d68edf42c
yuki-zirushi <yuko.12.151@gmail.com>
2021-04-17 05:36:39 +0000

    [Update] destroy

--- 3-7/CARAVAN/app/controllers/blogs_controller.rb
+++ 3-7/CARAVAN/app/controllers/blogs_controller.rb
@@ -30,7 +30,11 @@ class BlogsController < ApplicationController
   end
   
   def destroy
+    blog = Blog.find(params[:id])
+    blog.destroy
+    redirect_to blogs_path
     
+  end
   
   private
   def blog_params
